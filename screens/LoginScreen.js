8e6e3d1240b90b52530391373c91330811924bef
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-04-14 05:06:11 +0800

    Finish Login UI

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,35 +3,59 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
   TextInput,
-  Button,
+  TouchableOpacity,
   ActivityIndicator,
 } from 'react-native'
+
+//layout
 import MainContainer from '../layouts/MainContainer'
+
+//logo
+import logo from '../assets/logo.png'
+import flogo from '../assets/ficon.png'
+import glogo from '../assets/gicon.png'
 
 export default function LoginScreen() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
+  const [isLoadingIn, setIsLoadingIn] = useState(false)
 
   useEffect(() => {
     if (isLoading) {
       setTimeout(() => {
         handleLogin()
-      }, 2000)
+      }, 1000)
     }
   }, [isLoading])
+
+  useEffect(() => {
+    if (isLoadingIn) {
+      setTimeout(() => {
+        setIsLoggedIn(true)
+      }, 1000)
+    }
+  }, [isLoadingIn])
 
   const handleLogin = () => {
     if (username === 'admin' && password === 'admin') {
       setIsLoggedIn(true)
     } else {
       alert('Invalid username or password')
+      setIsLoading(false)
     }
   }
 
   const handleButtonPress = () => {
+    setIsLoading(true)
+  }
+  const handleButton = () => {
+    setIsLoadingIn(true)
+  }
+  const handleRegForm = () => {
     setIsLoading(true)
   }
 
@@ -42,7 +66,15 @@ export default function LoginScreen() {
 
   return (
     <View style={styles.container}>
+      <Image style={styles.tinyLogo} source={logo} />
+      {isLoading && (
+        <ActivityIndicator size='large' color='#FF8C00' position='absolute' />
+      )}
+      {isLoadingIn && (
+        <ActivityIndicator size='large' color='#FF8C00' position='absolute' />
+      )}
       <Text style={styles.title}>Login</Text>
+
       <TextInput
         style={styles.input}
         placeholder='Username'
@@ -56,8 +88,28 @@ export default function LoginScreen() {
         value={password}
         secureTextEntry={true}
       />
-      <Button style={styles.btn} title='Login' onPress={handleButtonPress} />
-      {isLoading && <ActivityIndicator size='large' color='#0000ff' />}
+      <TouchableOpacity style={styles.buttonReg} onPress={handleRegForm}>
+        <Text style={styles.textReg}>
+          Don't have an account?
+          <Text style={{ color: '#FF8C00' }}> Sign up</Text>
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttonLogin} onPress={handleButtonPress}>
+        <Text style={styles.buttonText}>LOGIN</Text>
+      </TouchableOpacity>
+
+      <Text style={{ marginTop: 50, marginBottom: 10 }}>
+        Or login with social account
+      </Text>
+      <View style={styles.iconContainer}>
+        <TouchableOpacity style={styles.iconContainer1} onPress={handleButton}>
+          <Image style={styles.icons} source={glogo} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer1} onPress={handleButton}>
+          <Image style={styles.icons} source={flogo} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -65,28 +117,75 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: 100,
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#fff',
   },
+  tinyLogo: {
+    height: 250,
+    width: 250,
+  },
   title: {
-    fontSize: 24,
+    fontSize: 34,
+    fontWeight: 'bold',
     marginBottom: 24,
+    alignSelf: 'flex-start',
+    marginLeft: 20,
   },
   input: {
     borderWidth: 1,
     borderColor: '#ccc',
     padding: 8,
     margin: 8,
-    width: '80%',
+    width: '85%',
     borderRadius: 4,
   },
-  btn: {
-    borderWidth: 1,
+  buttonLogin: {
+    marginTop: 40,
     padding: 8,
     margin: 8,
-    width: '80%',
+    width: '85%',
     borderRadius: 4,
     backgroundColor: '#FF8C00',
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  buttonText: {
+    color: 'white',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+  buttonReg: {
+    alignSelf: 'flex-end',
+    marginRight: '8%',
+  },
+  icons: {
+    height: 30,
+    width: 30,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    gap: 25,
+  },
+  iconContainer1: {
+    borderRadius: 20,
+    backgroundColor: '#FFf',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
   },
 })
