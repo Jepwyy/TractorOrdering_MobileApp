66a1fc2a622b48d84af2d49a94c3e8c89515474c
JM Familgan <jmfamilgan@gmail.com>
2023-04-16 10:57:28 +0800

    Notif and Prof Screen

--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -1,23 +1,106 @@
 import React from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  TouchableOpacity,
-  ActivityIndicator,
-} from 'react-native'
+import {StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, TouchableOpacity, ActivityIndicator,} from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+
 export default function NotificationScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text
-        onPress={() => alert('This is the "Home" screen.')}
-        style={{ fontSize: 26, fontWeight: 'bold' }}
-      >
-        notif Screen
-      </Text>
+    <View style={styles.parentContainer}>
+
+      <View style = {styles.lineTop}>
+        <Ionicons size={40} flex ={1} name="chevron-back-outline"></Ionicons>
+        <Text flex ={2} style = {styles.Title}>Notifications</Text>
+      </View>
+
+      <View style = {styles.line}>
+
+        <View paddingLeft = {'1%'} color={'#FF6600'}>
+          <Ionicons size={50} flex = {1} name="basket" onPress={() => alert('Will now proceed to "Promotions" Tab')}></Ionicons>
+        </View>
+
+        <View flex = {2} paddingLeft = {'5%'}>
+          <Text style = {styles.Heading2} onPress={() => alert('Will now proceed to "Promotions" Tab')}>Promotions</Text>
+          <Text style = {styles.Heading3} onPress={() => alert('Will now proceed to "Promotions" Tab')}>Get this EXCLUSIVE voucher for 100% off when...</Text>
+        </View>
+
+        <View>
+        <Ionicons size={40} flex ={1} name="chevron-forward" onPress={() => alert('Will now proceed to "Promotions" Tab')}></Ionicons>
+        </View>
+
+      </View>
+
+
+      <View style = {styles.line}>
+
+        <View paddingLeft = {'2%'}>
+          <Ionicons size={40} name="time" onPress={() => alert('Will now proceed to "Activities" Tab')}></Ionicons>
+        </View>
+
+        <View flex = {2} paddingLeft = {'5%'}>
+          <Text style = {styles.Heading2} onPress={() => alert('Will now proceed to "Activities" Tab')}>Activities</Text>
+          <Text style = {styles.Heading3} onPress={() => alert('Will now proceed to "Activities" Tab')}>No activity yet</Text>
+        </View>
+
+        <View>
+          <Ionicons size={40} flex ={1} name="chevron-forward" onPress={() => alert('Will now proceed to "Activities" Tab')}></Ionicons>
+        </View>
+
+      </View>
+
     </View>
   )
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  parentContainer:{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems:'center',
+    paddingTop:20,
+  },
+
+
+  lineTop:{
+    flexDirection: 'row',
+    paddingVertical:'5%',
+    justifyContent: 'space-between',
+    alignItems:'center',
+    borderBottomColor: '#C3C3C3',
+    borderBottomWidth: 3,
+    width: '100%',
+  },
+
+  line:{    
+    flexDirection: 'row',
+    paddingVertical:'5%',
+    justifyContent: 'space-between',
+    alignItems:'center',
+    borderBottomColor: '#C3C3C3',
+    borderBottomWidth: 1,
+    width: '100%',
+  },
+
+content:{
+  paddingVertical:'5%',
+  justifyContent: 'flex-start',
+  alignItems:'center',
+},
+
+  Title:{
+    fontSize: 26,    
+  fontFamily:'serif',
+  },
+
+  Heading2:{
+    fontSize: 24,    
+    fontFamily:'serif',
+  },
+
+  Heading3:{
+    fontSize: 12,    
+  color:'#AAAAAA',
+  },
+
+
+})
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,16 +1,218 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-const ProfileScreen = () => {
+import {StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, TouchableOpacity, ActivityIndicator,} from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+import dp from '../assets/dp.jpg'
+
+export default function ProfileScreen () {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text
-        onPress={() => alert('This is the "Home" screen.')}
-        style={{ fontSize: 26, fontWeight: 'bold' }}
-      >
-        prof Screen
-      </Text>
+    
+    <View style = {styles.parentContainer}>
+      <ScrollView style = {styles.scrollView}>
+      <View style = {styles.row}>
+        <Text style = {styles.title}>My Profile</Text>
+      </View>
+      
+      <View style = {styles.rowLine}>
+
+        <View style = {styles.imgContainer}>
+          <Image style = {styles.image} source={dp}></Image>
+        </View>
+
+        <View style = {styles.row1}>
+          <Text style = {styles.h1}>Abdul Jabul</Text>
+          <Text style = {styles.h2}>abdul.jabul@gmail.com</Text>
+        </View>
+
+      </View>
+
+      <View style = {styles.row}>
+
+        <View style = {styles.row1} flex = {2}>
+          <Text style = {styles.h5}>My Purchases</Text>
+        </View>
+
+        <View paddingRight = {'5%'}>
+        <Ionicons size={20} flex ={1} name="chevron-forward"></Ionicons>
+        </View>
+
+      </View>
+
+      <View style = {styles.rows}>
+
+        <View style = {styles.row2} flex = {1}>
+          <Ionicons size={30} name="wallet"></Ionicons>
+          <Text>To Pay</Text>
+        </View>
+        <View style = {styles.row2} flex = {1}>
+          <Ionicons size={30} name="boat"></Ionicons>
+          <Text>To Ship</Text>
+        </View>
+        <View style = {styles.row2} flex = {1}>
+          <Ionicons size={30} name="log-in"></Ionicons>
+          <Text>To Recieve</Text>
+        </View>
+
+      </View>
+
+      <View style = {styles.rows}>
+        <View style = {styles.row1}  flex ={2}>
+          <Text style = {styles.h5}>Shipping addresses</Text>
+          <Text style = {styles.h6}>2 Addresses</Text>
+        </View>
+
+        <View paddingRight = {'5%'}>
+        <Ionicons size={20} flex ={1} paddingTop = {8} name="chevron-forward"></Ionicons>
+        </View>
+
+      </View>
+
+      <View style = {styles.rows}>
+        <View style = {styles.row1}   flex ={2}>
+          <Text style = {styles.h5}>Payment Methods</Text>
+          <Text style = {styles.h6}>Visa **89</Text>
+        </View>
+
+        <View paddingRight = {'5%'}>
+        <Ionicons size={20} flex ={1} paddingTop = {8} name="chevron-forward"></Ionicons>
+        </View>
+
+      </View>
+
+      <View style = {styles.rows}>
+        <View style = {styles.row1}  flex ={2}>
+          <Text style = {styles.h5}>Settings</Text>
+          <Text style = {styles.h6}>Name, Password</Text>
+        </View>
+
+        <View paddingRight = {'5%'}>
+        <Ionicons size={20} flex ={1} paddingTop = {8} name="chevron-forward"></Ionicons>
+        </View>
+
+      </View>
+
+      <View style = {styles.rows}>
+        <View style = {styles.row1}  flex ={2}>
+          <Text style = {styles.h5}>About Us</Text>
+          <Text style = {styles.h6}>Geramfer Commercial Corp.</Text>
+        </View>
+
+        <View paddingRight = {'5%'}>
+        <Ionicons size={20} flex ={1} paddingTop = {8} name="chevron-forward"></Ionicons>
+        </View>
+      </View>
+
+      <View style = {styles.rows}>
+        <View style = {styles.row1}  flex ={2}>
+          <Text style = {styles.logout}>Log Out</Text>
+        </View>
+
+        <View paddingRight = {'5%'}>
+        <Ionicons size={20} flex ={1} paddingTop = {8} name="chevron-forward"></Ionicons>
+        </View>
+
+      </View>
+      </ScrollView>
     </View>
+
   )
 }
+const styles = StyleSheet.create({
+  scrollView:{
+    width: '100%',
+},
 
-export default ProfileScreen
+  parentContainer:{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems:'center',
+    paddingTop:'20%',
+    marginTop:-20,
+  },
+  row:{
+    flexDirection: 'row',
+    paddingTop:'5%',
+    paddingLeft:'5%',
+    justifyContent: 'space-between',
+    alignItems:'center',
+    width: '100%',
+  },
+  rowLine:{
+    flexDirection: 'row',
+    paddingVertical:'5%',
+    paddingLeft:'5%',
+    justifyContent: 'space-between',
+    alignItems:'center',
+    width: '100%',
+    borderBottomColor: '#C3C3C3',
+    borderBottomWidth: 1,
+  },
+  rows:{
+    flexDirection: 'row',
+    paddingVertical:'5%',
+    justifyContent: 'space-between',
+    alignItems:'center',
+    width: '100%',
+    borderBottomColor: '#C3C3C3',
+    borderBottomWidth: 1,
+  },
+  row1:{
+    flexDirection: 'column',
+    paddingLeft:'5%',
+    justifyContent: 'center',
+    alignItems:'flex-start',
+    width: '100%',
+  },
+  row2:{
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems:'center',
+    width: '100%',
+  },
+title:{
+  fontSize: 40,
+  fontWeight:'bold',
+},
+imgContainer:{
+  width: 100,
+  height: 100,
+  borderRadius: 50,
+  overflow: 'hidden',
+},
+image: {
+  width: 100,
+  height: 100,
+},
+
+h1:{
+  fontSize: 20,
+  fontFamily:'serif',
+},
+h2: {
+  fontSize: 15,
+  fontWeight:'100',
+  fontFamily:'serif',
+  color:'#AAAAAA',
+},
+
+h5:{
+  fontFamily:'serif',
+  fontSize: 16,
+},
+
+h6: {
+  fontFamily:'serif',
+  fontSize: 12,
+  color:'#AAAAAA',
+  paddingTop:3,
+},
+
+logout:{
+  fontFamily:'serif',
+  fontSize: 20,
+},
+
+
+})
+
